a6a72b5a2f057c0558b72723d3b3f4e11bacfc98
Liam Magee <liam.magee@theconversation.edu.au>
2013-01-04 14:03:48 +1100

    Initial commit of CoSign Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cosign::Application.config.secret_token = '52f4f36c6b9b8a610e488ad18b6ea6ec412679a3096af78dc48b9bbdb6d18787faea80e2544766bed94d9a3cf9adb46631718c8d6f38f3687b4a729767b6caca'
